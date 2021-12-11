f0c61f164335be1a9001c5823e160bf6ae16bc14
zharfanwafiq <zharfanww@gmail.com>
2021-12-11 11:47:43 +0700

    insertViewGroupCaption

--- app/src/main/java/com/zharfan/ViewViewGroupActivity.kt
+++ app/src/main/java/com/zharfan/ViewViewGroupActivity.kt
@@ -2,11 +2,29 @@ package com.zharfan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.zharfan.belajarkotlin.R
+import com.zharfan.belajarkotlin.databinding.ActivityViewViewGroupBinding
+
 
 class ViewViewGroupActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityViewViewGroupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_view_group)
+        binding = ActivityViewViewGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Google Pixel"
+
+        setOnClickButton()
+    }
+
+    private fun setOnClickButton() {
+        binding.apply {
+            btnBuy.setOnClickListener{
+                Toast.makeText(this@ViewViewGroupActivity, "Silakan DI Beli", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
