d5e20be35d7d932852885b10ee9824872177f9d2
zharfanwafiq <zharfanww@gmail.com>
2021-12-12 15:07:42 +0700

    Remove viewgoup

---
+++ app/src/main/java/com/zharfan/StyleThemeActivity.kt
@@ -0,0 +1,12 @@
+package com.zharfan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zharfan.belajarkotlin.R
+
+class StyleThemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style_theme)
+    }
+}
